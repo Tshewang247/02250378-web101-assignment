007cb11387c2b1fd01d46546d49d96b4137b6e27
Tshewang Samdup <02250378.cst@rub.edu.bt>
2026-03-17 14:31:56 +0600

    fourth

--- tiktok/src/components/layout/MainLayout.jsx
+++ tiktok/src/components/layout/MainLayout.jsx
@@ -7,7 +7,7 @@ export default function MainLayout({ children }) {
   const pathname = usePathname();
 
   return (
-    <div className="flex min-h-screen bg-gray-50 dark:bg-black">
+    <div className="flex min-h-screen bg-white dark:bg-black">
       {/* Sidebar */}
       <aside className="w-64 bg-white dark:bg-gray-900 p-6 flex flex-col border-r border-gray-200 dark:border-gray-700">
         <h1 className="text-xl font-bold mb-6 text-black dark:text-white">
